41993148b76dbd0154a561a54215aa8dc7733992
2019007901 <aus.baik@gmail.com>
2020-05-07 07:21:16 +0900

    Project08_2

---
+++ Project08_2/src/selfTest/ExpiredSale.java
@@ -0,0 +1,33 @@
+package selfTest;
+
+public class ExpiredSale extends Sale {
+	
+	private double expired;
+	
+	public ExpiredSale() {
+		super();
+		setExpired(0);
+	}
+	
+	public ExpiredSale(String theName, double thePrice, double theExpired) {
+		super(theName, thePrice);
+		setExpired(theExpired);
+	}
+	
+	public void setExpired(double theExpired) {
+		expired = theExpired;
+	}
+	
+	public String toString() { return (getName() + " Price = $" + getPrice() + " Expired = " + expired + " left\n" + " Deliver Fee = $" + deliverFee()); }
+	
+	public double deliverFee() {
+		if(expired >= 10) return 3;
+		else if (expired<10 && expired>=3) return 2;
+		else if (expired<3 && expired>=1) return 0;
+		else {
+			System.out.println("Cannot be delivered");
+			return -1;
+		}
+	}
+	
+}
